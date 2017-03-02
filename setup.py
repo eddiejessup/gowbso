c307bce02e8d0ec704ce30d7bd49dae1adb0244a
Elliot Marsden <elliot.marsden@booking.com>
2017-03-02 16:39:43 +0100

    Make everything gowbso

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'wbso = wbso.wbso:main',
+            'gowbso = gowbso.gowbso:main',
         ],
     },
 )
